bce51e6b1aed3d560c5c38bb2d0be25ae42bb00c
Shashank Gupta <shashankgupta1455@gmail.com>
2024-07-15 14:29:45 +0530

    AG grid

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { AgGridReact } from 'ag-grid-react';
+import "ag-grid-community/styles/ag-grid.css"; 
+import "ag-grid-community/styles/ag-theme-quartz.css";
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div>
+      <h1>Vite + React</h1>
+      </div>
+    </>
+  )
+}
+
+export default App
